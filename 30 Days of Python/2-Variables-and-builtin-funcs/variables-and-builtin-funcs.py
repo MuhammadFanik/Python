2b2e7ae22e3d8b627efd2e0fedfbec693a788d2a
Muhammad Fanik <muhammadfanik0@gmail.com>
2026-06-18 01:06:11 +0500

    Day 2 complete

---
+++ 30 Days of Python/2-Variables-and-builtin-funcs/variables-and-builtin-funcs.py
@@ -0,0 +1,42 @@
+# Day 2: 30 Days of programming
+
+
+# Level 1
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+country = "Spain"
+city = "Barcelona"
+age = 18
+year = 2026
+is_married = True
+is_true = True
+is_light_on = False
+var1, var2, var3 = 1, 10, 100
+
+# Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print("\n")
+print(len(first_name))
+print(len(first_name), len(last_name))
+
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_div = num_one // num_two
